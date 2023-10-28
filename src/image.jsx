69297fe794de56f3af3ee673c2fe08eef1a25da1
westempire <westempire@westempire>
2023-10-28 16:23:51 +0100

    completed the desktop screen for home page and entered mobile screen

---
+++ src/image.jsx
@@ -0,0 +1,30 @@
+import curvedline from "./assets/images/Vector 4.png";
+import hubpic from "./assets/images/Title.png";
+import grouped from "./assets/images/Grouped.png";
+import bulb from "./assets/images/idea.png";
+import gosip from "./assets/images/gosip.png";
+import family from "./assets/images/family.png";
+import thought from "./assets/images/thought.png";
+import first from "./assets/images/first.png";
+import second from "./assets/images/second.png";
+import third from "./assets/images/third.png";
+import fourth from "./assets/images/fourth.png";
+import fifth from "./assets/images/fifth.png";
+import sixth from "./assets/images/sixth.png";
+import cup from "./assets/images/cup.png"
+import reward from "./assets/images/Rewards.png"
+import partner from "./assets/images/Partner.png"
+import secure from "./assets/images/secure.png";
+import call from "./assets/images/call.png";
+import x from "./assets/images/x.png";
+import fb from "./assets/images/fb.png";
+import location from "./assets/images/location.png";
+import instagram from "./assets/images/instagram.png";
+import linkedin from "./assets/images/likedin.png"
+import logo from "./assets/images/getlinked.png";
+import menu from './assets/images/menu.png';
+import cancel from './assets/images/cancel.png'
+
+
+
+export {curvedline, hubpic,grouped,gosip,bulb,family,thought,first,second,third,fourth,fifth,sixth,cup,reward,partner,secure,call,x,fb,location,instagram,linkedin,logo,menu,cancel}
